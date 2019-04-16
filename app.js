86c1f37ff7a87d6dc3fd9467856dd978e19330f5
Josiah Ngu <jos81498@gmail.com>
2019-04-15 21:35:58 -0500

    fix some of the eslint rule

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+let server;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+/* eslint-disable func-names */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +40,7 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
-var server = app.listen(3000, function() {
+server = app.listen(3000, function() {
   console.log("Calling app.listen's callback function.");
   const host = server.address().address;
   const port = server.address().port;
